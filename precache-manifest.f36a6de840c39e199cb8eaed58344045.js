8b81744291b5767bacd180a860f75fabc82dcd7a
danRalph <dan.r.richards@hotmail.co.uk>
2020-06-06 14:38:37 +0100

    Updates

---
+++ precache-manifest.f36a6de840c39e199cb8eaed58344045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695c7fbfb39f64ba0e27f7d7f0f6b28c",
+    "url": "/Unused-Portfolio/index.html"
+  },
+  {
+    "revision": "39733bb5c598f002a918",
+    "url": "/Unused-Portfolio/static/css/main.f94bfabb.chunk.css"
+  },
+  {
+    "revision": "f0f4fa43e28fbee86948",
+    "url": "/Unused-Portfolio/static/js/2.d4703551.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Unused-Portfolio/static/js/2.d4703551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39733bb5c598f002a918",
+    "url": "/Unused-Portfolio/static/js/main.d94917d7.chunk.js"
+  },
+  {
+    "revision": "707e46b88ca86c3719e0",
+    "url": "/Unused-Portfolio/static/js/runtime-main.91f397e3.js"
+  }
+]);
